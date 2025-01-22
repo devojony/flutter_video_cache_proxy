20fdc1eea72c08d07449bb4287a3e502ee28025c
xuyunchuan <xs199153@163.com>
2025-01-22 16:30:20 +0800

    更新基础代码

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:media_kit/media_kit.dart'; // Provides [Player], [Media], [Playlist] etc.
 import 'package:media_kit_video/media_kit_video.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Necessary initialization for package:media_kit.
   MediaKit.ensureInitialized();
-
 
   runApp(
     const MaterialApp(
@@ -25,22 +23,27 @@ class MyScreen extends StatefulWidget {
 class MyScreenState extends State<MyScreen> {
   // Create a [Player] to control playback.
   late final player = Player(
-    configuration: const PlayerConfiguration(
-      bufferSize: 1024 * 1024,
-    ),
+    configuration: const PlayerConfiguration(bufferSize: 1024 * 1024, logLevel: MPVLogLevel.trace),
   );
   // Create a [VideoController] to handle video output from [Player].
   late final controller = VideoController(player);
+
+  final urls = [
+    'https://user-images.githubusercontent.com/28951144/229373695-22f88f13-d18f-4288-9bf1-c3e078d83722.mp4',
+    'https://user-images.githubusercontent.com/28951144/229373709-603a7a89-2105-4e1b-a5a5-a6c3567c9a59.mp4',
+    'https://user-images.githubusercontent.com/28951144/229373716-76da0a4e-225a-44e4-9ee7-3e9006dbc3e3.mp4',
+    'https://user-images.githubusercontent.com/28951144/229373718-86ce5e1d-d195-45d5-baa6-ef94041d0b90.mp4',
+    'https://user-images.githubusercontent.com/28951144/229373720-14d69157-1a56-4a78-a2f4-d7a134d7c3e9.mp4',
+    'https://stream7.iqilu.com/10339/upload_transcode/202002/18/20200218114723HDu3hhxqIT.mp4',
+  ].map((e) => 'http://localhost:8080?url=$e');
 
   @override
   void initState() {
     super.initState();
     // Play a [Media] or [Playlist].
-    player.open(
-      Media(
-        'http://0.0.0.0:8080?url=https://stream7.iqilu.com/10339/upload_transcode/202002/18/20200218114723HDu3hhxqIT.mp4',
-      ),
-    );
+    final playlist = Playlist(urls.map((e) => Media(e)).toList(growable: false));
+    player.setPlaylistMode(PlaylistMode.loop);
+    player.open(playlist);
   }
 
   @override
